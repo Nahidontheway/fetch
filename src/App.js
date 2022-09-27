eeda4d07ade4d8d57090803c851706f7e690ddeb
Nahidontheway <19203103053@cse.bubt.edu.bd>
2022-09-27 15:08:01 +0600

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
@@ -12,9 +13,20 @@ function App() {
 }
 
 function ExternalData(){
+  const [users, setUsers] = useState([])
+  // const arrow =() =>{}
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
   return(
     <div>
       <h2>External User</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <li>{user.name}</li>)
+      }
     </div>
   )
 }
