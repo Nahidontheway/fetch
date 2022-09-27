398273045ff6cf58d676f538096c8cb173b8cb2f
Nahidontheway <19203103053@cse.bubt.edu.bd>
2022-09-27 14:54:47 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ExternalData></ExternalData>
       </header>
     </div>
   );
+}
+
+function ExternalData(){
+  return(
+    <div>
+      <h2>External User</h2>
+    </div>
+  )
 }
 
 export default App;
